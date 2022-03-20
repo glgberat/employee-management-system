8d77e90974b00b503b434f26ee097d77272e8f0d
Abbush <glgberat@yahoo.com>
2022-03-19 22:49:06 -0400

    schema.sql role_id constraint fk error has been fixed

--- db/schema.sql
+++ db/schema.sql
@@ -28,7 +28,7 @@ first_name VARCHAR(30) NOT NULL,
 last_name VARCHAR(30) NOT NULL,
 role_id INTEGER NOT NULL,
 manager_id INTEGER ,
-CONSTRAINT fk_role FOREIGN KEY (role_id) REFERENCES role(id) ON DELETE SET NULL,
+CONSTRAINT fk_role FOREIGN KEY (role_id) REFERENCES role(id) ON DELETE CASCADE,
 CONSTRAINT fk_mngid FOREIGN KEY (manager_id) REFERENCES employee(id) ON DELETE SET NULL
 
 );
